d59d92edc7d85f623deea430d0885160fdb23f05
Nick Pezza <npezza93@gmail.com>
2018-12-05 23:51:03 -0500

    Use window on load event

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -80,6 +80,6 @@ $(document).one('focus.expand', 'textarea.expand', function() {
   this.rows = minRows + rows;
 });
 
-$(document).ready(function() {
+window.addEventListener('load', function(event) {
   new MDCTabScroller(document.querySelector('.mdc-tab-scroller'))
 })
