db9fdfbfef1fad6f94eef608ae33cca85e13240c
Nick Pezza <npezza93@gmail.com>
2020-06-20 11:54:15 -0400

    Add recaptcha

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class EmailsController < ApplicationController
+  RECAPTCHA_MINIMUM_SCORE = 0.5
+
   def create
+    return unless valid_recaptcha?(params[:recaptcha_token])
+
     @email = Email.new(email_params)
 
     @email.save
@@ -11,5 +15,15 @@ class EmailsController < ApplicationController
 
   def email_params
     params.require(:email).permit(:name, :email, :message)
+  end
+
+  def valid_recaptcha?(token)
+    secret_key = ENV["RECAPTCHA_SECRET_KEY"]
+
+    uri = URI.parse("https://www.google.com/recaptcha/api/siteverify?secret=#{secret_key}&response=#{token}")
+    response = Net::HTTP.get_response(uri)
+    json = JSON.parse(response.body)
+    json["success"] && json["score"] > RECAPTCHA_MINIMUM_SCORE &&
+      json["action"] == "email"
   end
 end
--- test/controllers/email_controller_test.rb
+++ test/controllers/email_controller_test.rb
@@ -1,14 +1,36 @@
 # frozen_string_literal: true
 
 require "test_helper"
+require "minitest/autorun"
 
 class EmailControllerTest < ActionDispatch::IntegrationTest
+  test "should not create" do
+    response_mock = Minitest::Mock.new.expect(
+      :body, { "success": false, "score": 1, "action": "email" }.to_json
+    )
+
+    Net::HTTP.stub(:get_response, response_mock) do
+      assert_no_difference -> { ActionMailer::Base.deliveries.size } do
+        post emails_url, params: { email: {
+          name: "name", email: "email@email.com", message: "message"
+        } }
+        assert_response :success
+      end
+    end
+  end
+
   test "should create" do
-    assert_difference -> { ActionMailer::Base.deliveries.size } do
-      post emails_url, params: { email: {
-        name: "name", email: "email@email.com", message: "message"
-      } }
-      assert_response :success
+    response_mock = Minitest::Mock.new.expect(
+      :body, { "success": true, "score": 1, "action": "email" }.to_json
+    )
+
+    Net::HTTP.stub(:get_response, response_mock) do
+      assert_difference -> { ActionMailer::Base.deliveries.size } do
+        post emails_url, params: { email: {
+          name: "name", email: "email@email.com", message: "message"
+        } }
+        assert_response :success
+      end
     end
   end
 end
